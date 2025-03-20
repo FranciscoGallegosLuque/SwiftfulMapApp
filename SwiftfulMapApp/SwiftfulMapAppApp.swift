38a8322f0eb307acf6dc64c9caa5044c0b19df73
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-03-20 18:12:45 -0300

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Francisco Manuel Gallegos Luque on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulMapAppTests/SwiftfulMapAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppTests.swift
+//  SwiftfulMapAppTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 20/03/2025.
+//
+
+import Testing
+@testable import SwiftfulMapApp
+
+struct SwiftfulMapAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
